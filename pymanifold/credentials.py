fa6b273936a71e69efd32afc7770e69ece6dd33e
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2022-10-16 08:57:49 -0400

    workflow credentials

--- pymanifold/credentials.py
+++ pymanifold/credentials.py
@@ -3,34 +3,18 @@ import os
 import keyring
 
 # %%
-MANIFOLD_USERNAME = os.environ.get("MANIFOLD_USERNAME") or keyring.get_password(
-    "manifold.markets", "MANIFOLD_USERNAME"
-)
-MANIFOLD_APIKEY = os.environ.get("MANIFOLD_APIKEY") or keyring.get_password(
-    "manifold.markets", "MANIFOLD_APIKEY"
-)
+MANIFOLD_USERNAME = os.environ.get("MANIFOLD_USERNAME")
+MANIFOLD_APIKEY = os.environ.get("MANIFOLD_APIKEY")
 
 
 def set_credentials(username, api_key):
     assert all([username, api_key]), "you should insert a valid username and api_key"
-
-    keyring.set_password("manifold.markets", "MANIFOLD_USERNAME", username)
-    keyring.set_password("manifold.markets", "MANIFOLD_APIKEY", api_key)
     os.environ["MANIFOLD_USERNAME"] = username
     os.environ["MANIFOLD_APIKEY"] = api_key
 
 
 def get_credentials():
-    username = os.environ.get("MANIFOLD_USERNAME") or keyring.get_password(
-        "manifold.markets", "MANIFOLD_USERNAME"
-    )
-    api_key = os.environ.get("MANIFOLD_APIKEY") or keyring.get_password(
-        "manifold.markets", "MANIFOLD_APIKEY"
-    )
-    try:
-        assert all([username, api_key]), "set your api key using environ or keyring"
-    except AssertionError:
-        username = input("MANIFOLD_USERNAME")
-        api_key = input("MANIFOLD_APIKEY")
-        set_credentials(username, api_key)
+    username = os.environ.get("MANIFOLD_USERNAME")
+    api_key = os.environ.get("MANIFOLD_APIKEY")
+    assert all([username, api_key]), "set your api key using environ"
     return username, api_key
