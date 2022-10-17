54c283a2b1398323120c88ba7f1634d63a7e0478
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2022-10-16 23:18:13 -0400

    python-dotenv

--- pymanifold/credentials.py
+++ pymanifold/credentials.py
@@ -1,8 +1,9 @@
 import os
+import typing as t
+from dotenv import load_dotenv
+from pathlib import Path
 
 # %%
-MANIFOLD_USERNAME = os.environ.get("MANIFOLD_USERNAME")
-MANIFOLD_APIKEY = os.environ.get("MANIFOLD_APIKEY")
 
 
 def set_credentials(username, api_key):
@@ -11,8 +12,18 @@ def set_credentials(username, api_key):
     os.environ["MANIFOLD_APIKEY"] = api_key
 
 
-def get_credentials():
+def get_credentials(dotenv_path=None):
+    if dotenv_path:
+        load_dotenv(Path(dotenv_path))
+
     username = os.environ.get("MANIFOLD_USERNAME")
     api_key = os.environ.get("MANIFOLD_APIKEY")
-    assert all([username, api_key]), "set your api key using environ"
+    if not all([username, api_key]):
+        try:
+            username, api_key = get_credentials(dotenv_path=Path.cwd()/'.env')
+        except Exception as e:
+            print('error loading dotenv')
+    assert all(
+        [username, api_key]
+    ), "you should set variables MANIFOLD_USERNAME and MANIFOLD_APIKEY on your enviroment"
     return username, api_key
