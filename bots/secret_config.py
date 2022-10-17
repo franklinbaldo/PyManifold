0f4883ac123266792bdc3644a1505f9959a41b14
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2022-10-17 20:50:57 -0300

    Group

--- bots/secret_config.py
+++ bots/secret_config.py
@@ -13,8 +13,17 @@ GROUPS = {
         #                                                    |  Required
         #                                                    |  |
         "will-lula-da-silva-win-the-2022-bra": [0, 1],
-        "will-lula-da-silva-win-the-2022-pre": [0, 1],
+        #"will-lula-da-silva-win-the-2022-pre": [0, 1],
         "lula-vai-ser-eleito-presidente-do-b": [0, 1],
+        #"will-jair-bolsonaro-be-reelected-pr": [1, 0],
+    },
+"Brazilian Elections2": {
+        #                                                    Not required
+        #                                                    |  Required
+        #                                                    |  |
+        "will-lula-da-silva-win-the-2022-bra": [0, 1],
+        "will-lula-da-silva-win-the-2022-pre": [0, 1],
+        #"lula-vai-ser-eleito-presidente-do-b": [0, 1],
         #"will-jair-bolsonaro-be-reelected-pr": [1, 0],
     },
 "Inverse Br election":{"lula-vai-ser-eleito-presidente-do-b": [0, 1],
